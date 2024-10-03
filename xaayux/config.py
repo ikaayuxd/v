999f4e61c845411f203e44dbab61d1772ee1ba91
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-10-03 21:33:26 +0530

    Update config.py

--- xaayux/config.py
+++ xaayux/config.py
@@ -2,5 +2,5 @@ API_ID = ('22461292')
 API_HASH = ('c246259fcfc0d13d2799cd182857e7e1')
 SESSION = ('1BZWaqwUAUIJ1OssE1Bq6mjjbGFhZGOkdp45vTb32hcUS_yMCjBjl7d3WCblreCFWkxgjYxebsTSTDT60Pvqlw4sc6wLvr5Afj_0cr7G-3cGcuB63EP8YMb_RarDF-5mlm9YCQCVI8abHZbmYBi9doUOAHoHRkZyJbDL-Q6M1AwIXD6LZvNgdvGevxdUZjrJyuig7HtgoqAcN0q8GM5bsU72u8CKqjL_uXqfyjJbyI4PCEAjjEmi2ezp55XkWZUlIHkdQDp6mU5SbT98LCwXqqH3DHyB7kgXJRfxhH3HLG_c66Fw4VPEWNRNHsuMCO2B0mQV6axmTvPtn8tsrB9ojF6PK_YcFvXw=')
 DELAY = 1800 #in seconds 
-channel_ids = [-1001966404031]
+channel_ids = [-1002434889437, -1002306529669, -1002417593367]
 messages = ["https://t.me/c/2043259921/58", "https://t.me/c/2043259921/58"]  # Add your desired message links here
